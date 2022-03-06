93cc786c7e3ef2fe5f169d2fc91ff632988e5bfb
laurenkearney00 <c18330401@mytudublin.ie>
2022-03-06 14:29:22 +0000

    Created AdminGUI to have components all in one class

--- RefactoringAssignment/src/redmondn/Admin.java
+++ RefactoringAssignment/src/redmondn/Admin.java
@@ -5,57 +5,7 @@ public class Admin extends Menu {
 	
 	dispose();
 	
-	f = new JFrame("Administrator Menu");
-	f.setSize(400, 400);
-	f.setLocation(200, 200);
-	f.addWindowListener(new WindowAdapter() {
-		public void windowClosing(WindowEvent we) { System.exit(0); }
-	});          
-	f.setVisible(true);
 	
-	JPanel deleteCustomerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton deleteCustomer = new JButton("Delete Customer");	
-	deleteCustomer.setPreferredSize(new Dimension(250, 20));
-	deleteCustomerPanel.add(deleteCustomer);
-	
-	JPanel deleteAccountPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton deleteAccount = new JButton("Delete Account");
-	deleteAccount.setPreferredSize(new Dimension(250, 20));	
-	deleteAccountPanel.add(deleteAccount);
-	
-	JPanel bankChargesPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton bankChargesButton = new JButton("Apply Bank Charges");
-	bankChargesButton.setPreferredSize(new Dimension(250, 20));	
-	bankChargesPanel.add(bankChargesButton);
-	
-	JPanel interestPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton interestButton = new JButton("Apply Interest");
-	interestPanel.add(interestButton);
-	interestButton.setPreferredSize(new Dimension(250, 20));
-	
-	JPanel editCustomerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton editCustomerButton = new JButton("Edit existing Customer");
-	editCustomerPanel.add(editCustomerButton);
-	editCustomerButton.setPreferredSize(new Dimension(250, 20));
-	
-	JPanel navigatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton navigateButton = new JButton("Navigate Customer Collection");
-	navigatePanel.add(navigateButton);
-	navigateButton.setPreferredSize(new Dimension(250, 20));
-	
-	JPanel summaryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton summaryButton = new JButton("Display Summary Of All Accounts");
-	summaryPanel.add(summaryButton);
-	summaryButton.setPreferredSize(new Dimension(250, 20));
-	
-	JPanel accountPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	JButton accountButton = new JButton("Add an Account to a Customer");
-	accountPanel.add(accountButton);
-	accountButton.setPreferredSize(new Dimension(250, 20));
-	
-	JPanel returnPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-	JButton returnButton = new JButton("Exit Admin Menu");
-	returnPanel.add(returnButton);
 
 	JLabel label1 = new JLabel("Please select an option");
 	
@@ -939,7 +889,7 @@ boolean found = true, loop = true;
 	returnButton.addActionListener(new ActionListener(  ) {
 		public void actionPerformed(ActionEvent ae) {
 			f.dispose();
-			//menuStart();				
+			menuStart();				
 		}
      });		
 
---
+++ RefactoringAssignment/src/redmondn/AdminGUI.java
@@ -0,0 +1,63 @@
+package redmondn;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class AdminGUI extends Menu {
+	public void adminGui() {
+	
+	f = new JFrame("Administrator Menu");
+	f.setSize(400, 400);
+	f.setLocation(200, 200);
+	f.addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent we) { System.exit(0); }
+	});          
+	f.setVisible(true);
+	
+	JPanel deleteCustomerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton deleteCustomer = new JButton("Delete Customer");	
+	deleteCustomer.setPreferredSize(new Dimension(250, 20));
+	deleteCustomerPanel.add(deleteCustomer);
+	
+	JPanel deleteAccountPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton deleteAccount = new JButton("Delete Account");
+	deleteAccount.setPreferredSize(new Dimension(250, 20));	
+	deleteAccountPanel.add(deleteAccount);
+	
+	JPanel bankChargesPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton bankChargesButton = new JButton("Apply Bank Charges");
+	bankChargesButton.setPreferredSize(new Dimension(250, 20));	
+	bankChargesPanel.add(bankChargesButton);
+	
+	JPanel interestPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton interestButton = new JButton("Apply Interest");
+	interestPanel.add(interestButton);
+	interestButton.setPreferredSize(new Dimension(250, 20));
+	
+	JPanel editCustomerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton editCustomerButton = new JButton("Edit existing Customer");
+	editCustomerPanel.add(editCustomerButton);
+	editCustomerButton.setPreferredSize(new Dimension(250, 20));
+	
+	JPanel navigatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton navigateButton = new JButton("Navigate Customer Collection");
+	navigatePanel.add(navigateButton);
+	navigateButton.setPreferredSize(new Dimension(250, 20));
+	
+	JPanel summaryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton summaryButton = new JButton("Display Summary Of All Accounts");
+	summaryPanel.add(summaryButton);
+	summaryButton.setPreferredSize(new Dimension(250, 20));
+	
+	JPanel accountPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	JButton accountButton = new JButton("Add an Account to a Customer");
+	accountPanel.add(accountButton);
+	accountButton.setPreferredSize(new Dimension(250, 20));
+	
+	JPanel returnPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	JButton returnButton = new JButton("Exit Admin Menu");
+	returnPanel.add(returnButton);
+
+}
+}
